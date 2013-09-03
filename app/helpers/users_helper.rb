9f6ab1c64c31caf0dbdcff104198f0e4a24eccf7
divine13 <dubedivine@gmail.com>
2013-09-03 16:34:47 +0200

    fixing the heroku problem. We're sorry, but something went wrong. at heroku

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,6 @@ module UsersHelper
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		size = options[:size]
 		gravatar_url = "divine.jpg?s=#{size}"
-		image_tag(gravatar_url,atl: user.name, class: "gravatar")
+		image_tag(gravatar_url, atl: user.name, class: "gravatar")
 	end 
 end
