174e3d5a145b8c55a243623575a388b3d599c2f8
divine13 <dubedivine@gmail.com>
2013-09-03 17:48:34 +0200

    removed the error prone method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
 	def gravatar_for(user, options = {size: 50})
-		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		size = options[:size]
-		gravatar_url = "rails.png?s=#{size}"
-		image_tag(gravatar_url, atl: user.name, class: "gravatar")
+	    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+	    size = options[:size]
+	    gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar id}.png?s=#{size}"
+	    image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end 
 end
