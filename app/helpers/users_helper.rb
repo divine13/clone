b161d77d00383bc4949d7df1d4edce27b7251ac5
divine13 <dubedivine@gmail.com>
2013-09-03 17:29:33 +0200

    fixed minor problem

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 	def gravatar_for(user, options = {size: 50})
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		size = options[:size]
-		gravatar_url = "divine.jpg?s=#{size}"
+		gravatar_url = "rails.png?s=#{size}"
 		image_tag(gravatar_url, atl: user.name, class: "gravatar")
 	end 
 end
